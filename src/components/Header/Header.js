5bc04957d41f24a6c496a28e69e87f2c71cbdbc0
unknown <freelancerfathe@gmail.com>
2022-04-17 15:58:32 +0600

    finised

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <ul className="nav justify-content-center text-center flex-column my-5 bg-dark py-3 text-white">
+      <h2>How to Get values and structure from checkbox and radio button</h2>
+      <h5>Learn Unique with fathe</h5>
+    </ul>
+  );
+};
+
+export default Header;
